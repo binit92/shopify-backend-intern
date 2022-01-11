8a37c07433010cd49d29a62c1c9fd0f12ff75211
Binit Kumar <kumar.binit1992@gmail.com>
2022-01-11 14:12:35 -0500

    Dev: Adding the initial code that uses flask to add inventory into local SQLlite database named test.db . I have also added basic templates and css files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shopify-backend-intern
 requirement for summer internship 2022
+
+
+conda install flask flask-sqlalchemy
---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# __name
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+#decorator
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        inv_content = request.form['content']
+        new_inv = Inventory(content = inv_content)
+
+        try:
+            db.session.add(new_inv)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding inventory'
+        
+    else:
+        all_inventory = Inventory.query.order_by(Inventory.date_created).all()
+
+        return render_template('index.html', all_inventory = all_inventory)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    inv_to_delete = Inventory.query.get_or_404(id)
+
+    try:
+        db.session.delete(inv_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that inventory'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+
+    inv = Inventory.query.get_or_404(id)
+
+    if request.method == 'POST':
+        inv.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue updating your inventory"
+    else:
+        return render_template('update.html', inv=inv)
+
+if __name__ == "__main__":
+    app.run(debug=True)
